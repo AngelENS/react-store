5b0086cf8c5553db806bcfe3e61aa0e9f278a3ad
Angel Nuñez <angel_9819@live.com.mx>
2021-03-17 18:25:24 -0500

    Diseño e implementacion de Middlewares - Se diseñaron los middlewares log y toast para registrar operaciones y mostrar mensajes de error respetivamente - Se implemento el middleware Thunk del redux tool kit

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import configureStore from './store/configureStore';
-import { bugAdded, bugResolved, bugAssignedToUser, getUnresolvedBugs, getBugsByUser} from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    console.log("Store Change!");
-})
+/*store.dispatch((dispatch, getState) => {
+    dispatch({type: 'bugsReceived', bugs: [1, 2, 3]});
+    console.log(getState());
+})*/
 
-store.dispatch(userAdded({ name: 'User 1' }));
-store.dispatch(userAdded({ name: 'User 2' }));
-store.dispatch(bugAdded({ description: "BUG1" }));
-store.dispatch(bugAdded({ description: "BUG2" }));
-store.dispatch(bugAdded({ description: "BUG3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(projectAdded({ name: 'Project 1' }));
-
-const assignedBugs = getBugsByUser(1)(store.getState());
-console.log(assignedBugs);
-
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-console.log(unresolvedBugs);
+store.dispatch({
+    type:"error",
+    payload: {message: "An error ocurred."}
+});
